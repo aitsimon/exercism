74b63bfef5ba2177f36b33ceae2b2ca1800fe409
Aitor Simon Munoz <aitor.simon@globant.com>
2023-02-15 17:35:27 +0100

    build: first exercises of the js track

---
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -0,0 +1,56 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Determines whether or not you need a licence to operate a certain kind of vehicle.
+ *
+ * @param {string} kind
+ * @returns {boolean} whether a license is required
+ */
+export function needsLicense(kind) {
+  return kind ==='car' || kind === 'truck' ? true : false;
+}
+
+/**
+ * Helps choosing between two options by recommending the one that
+ * comes first in dictionary order.
+ *
+ * @param {string} option1
+ * @param {string} option2
+ * @returns {string} a sentence of advice which option to choose
+ */
+export function chooseVehicle(option1, option2) {
+  let words1 = option1.split(' ');
+  let words2 = option2.split(' ');
+  for (let index = 0; index < words1.length; index++) {
+    let comparation = words1[index].localeCompare(words2[index]);
+    if(comparation === 0){
+      continue;
+    }else if(comparation === 1){
+      return option2 + ' is clearly the better choice.';
+    }else {
+      return option1 + ' is clearly the better choice.';
+    }
+    }
+}
+
+/**
+ * Calculates an estimate for the price of a used vehicle in the dealership
+ * based on the original price and the age of the vehicle.
+ *
+ * @param {number} originalPrice
+ * @param {number} age
+ * @returns {number} expected resell price in the dealership
+ */
+export function calculateResellPrice(originalPrice, age) {
+  if(age<3){
+    return originalPrice * 0.8
+  }else if(age<=10){
+    return originalPrice * 0.7
+  }else{
+    return originalPrice * 0.5
+  }
+}

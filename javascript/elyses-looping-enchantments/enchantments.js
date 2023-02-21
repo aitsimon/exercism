9e4aebc107904c23d5f958f13834bf2292030aa1
Aitor Simon Munoz <aitor.simon@globant.com>
2023-02-21 10:13:54 +0100

    build(elyses-looping-enchantments)

---
+++ javascript/elyses-looping-enchantments/enchantments.js
@@ -0,0 +1,41 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  return stack.reduce((accumulator, currentValue) => {
+    if(currentValue === card){
+      accumulator++;
+    }
+    return accumulator;
+  }, 0);
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let count = 0;
+  for (let card of stack) {
+    if(type){
+      if(card % 2 === 0){
+        count++;
+      }
+    }else{
+      if(card % 2 !== 0){
+        count++;
+      }
+    }
+  }
+  return count;
+}
